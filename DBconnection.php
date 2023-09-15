823ead38d86ed68331e30c89db58699ee67faebd
haider <haiderraz77@gmail.com>
2023-09-15 16:32:48 +0300

    done

--- DBconnection.php
+++ DBconnection.php
@@ -1,9 +1,17 @@
 <?php
-try{
-    $pdo=new pdo("mysql:host=localhost;dbname=php_basics;charset=utf8",'root','haiderrazaq');
-}catch(PDOException $e){
-    echo $e->getMessage();
+class DBconnection{
+    private static $pdo;
+    public static function make(){
+        
+        try{ 
+            self::$pdo=self::$pdo ? :new pdo("mysql:host=localhost;dbname=php_basics;charset=utf8",'root','haiderrazaq');
+        }catch(PDOException $e){
+            echo $e->getMessage();
+        }
+    }
+
 }
+DBconnection::make();
 
 
 
