cd942fe922b2237969434fb96776bd3f622c5fd5
haider <haiderraz77@gmail.com>
2023-09-15 16:45:09 +0300

    upgrade

--- DBconnection.php
+++ DBconnection.php
@@ -1,19 +1,20 @@
 <?php
-class DBconnection{
+class DBconnection
+{
     private static $pdo;
-    public static function make(){
-        
-        try{ 
-            self::$pdo=self::$pdo ? :new pdo("mysql:host=localhost;dbname=php_basics;charset=utf8",'root','haiderrazaq');
-        }catch(PDOException $e){
+    private static $dbname = 'php_basics';
+    private static $user = 'root';
+    private static $password = 'haiderrazaq';
+    public static function make()
+    {
+        $dbname = self::$dbname;
+        $user = self::$user;
+        $password = self::$password;
+        try {
+            self::$pdo = self::$pdo ?: new pdo("mysql:host=localhost;dbname=$dbname;charset=utf8", $user, $password);
+        } catch (PDOException $e) {
             echo $e->getMessage();
         }
     }
-
 }
 DBconnection::make();
-
-
-
-
-?>
