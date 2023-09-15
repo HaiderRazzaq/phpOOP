8542193b0c0c589875facac8879360d896ea81cb
haider <haiderraz77@gmail.com>
2023-09-15 16:25:54 +0300

    try catch for error handling

--- DBconnection.php
+++ DBconnection.php
@@ -1,5 +1,9 @@
-<?php 
-$pdo=new pdo("mysql:host=localhost;dbname=php_basics;charset=utf8",'root','haiderrazaq');
+<?php
+try{
+    $pdo=new pdo("mysql:host=localhost;dbname=php_basics;charset=utf8",'root','haiderrazaq');
+}catch(PDOException $e){
+    echo $e->getMessage();
+}
 
 
 
